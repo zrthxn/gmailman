4d5cba5b58db1c5c007e5f80ff9a3e34a4ed0fdd
Alisamar Husain <zrthxn@gmail.com>
2020-07-07 16:24:44 +0530

    x

--- src/util/init.ts
+++ src/util/init.ts
@@ -6,13 +6,16 @@ import { conf } from './conf'
 
 console.log(conf.Blue('Creating MAIL directory in cwd...'))
 
+const ROOTDIR = path.resolve('../../../', MAILDIR)
 const _init_config_ = JSON.stringify({
   accounts: {}, templates: {}
 }, null, 2)
 
-fs.mkdirSync(path.resolve(MAILDIR, '../../../'), { recursive: true })
-fs.mkdirSync(path.join(path.resolve(MAILDIR, '../../../'), 'auth'), { recursive: true })
-fs.mkdirSync(path.join(path.resolve(MAILDIR, '../../../'), 'templates'), { recursive: true })
+console.log(ROOTDIR)
+
+fs.mkdirSync(ROOTDIR, { recursive: true })
+fs.mkdirSync(path.join(ROOTDIR, 'auth'), { recursive: true })
+fs.mkdirSync(path.join(ROOTDIR, 'templates'), { recursive: true })
 
 fs.writeFileSync(path.join(path.resolve(MAILDIR, '../../../'), 'gmailer.config.json'), _init_config_)
 
