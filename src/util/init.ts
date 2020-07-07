1e9f8cb3b5110cc8e530911c6d574821c6997a87
Alisamar Husain <zrthxn@gmail.com>
2020-07-07 16:20:40 +0530

    Init

--- src/util/init.ts
+++ src/util/init.ts
@@ -10,10 +10,10 @@ const _init_config_ = JSON.stringify({
   accounts: {}, templates: {}
 }, null, 2)
 
-fs.mkdirSync(MAILDIR, { recursive: true })
-fs.mkdirSync(path.join(MAILDIR, 'auth'), { recursive: true })
-fs.mkdirSync(path.join(MAILDIR, 'templates'), { recursive: true })
+fs.mkdirSync(path.resolve(MAILDIR, '../../../'), { recursive: true })
+fs.mkdirSync(path.join(path.resolve(MAILDIR, '../../../'), 'auth'), { recursive: true })
+fs.mkdirSync(path.join(path.resolve(MAILDIR, '../../../'), 'templates'), { recursive: true })
 
-fs.writeFileSync(path.join(MAILDIR, 'gmailer.config.json'), _init_config_)
+fs.writeFileSync(path.join(path.resolve(MAILDIR, '../../../'), 'gmailer.config.json'), _init_config_)
 
 console.log(conf.Green('Initialized MAILDIR'))
